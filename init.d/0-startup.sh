eee8aaf6aa688ab3af2ecf820e6fb3db4acaac73
gjones.pf@gmail.com <gjones.pf@gmail.com>
2017-01-16 08:55:29 +1300

    Env related tweaks

--- init.d/0-startup.sh
+++ init.d/0-startup.sh
@@ -10,11 +10,31 @@ if [ -z "$APPLICATION_CNAME" ]; then
     ALTCNAME=$(echo $VIRTUAL_HOST| awk -F',' '{print $1}')
     echo "WARNING: No CNAME in env, using $ALTCNAME from VIRTUAL_HOST"
     export APPLICATION_CNAME=$ALTCNAME
+
+    touch /scripts/.apacheenv
+    if grep -Fxq "$ex" /scripts/.apacheenv
+    then
+        echo "CNAME already in .apacheenv"
+    else
+        ex="export APPLICATION_CNAME=$ALTCNAME"
+        echo "$ex" >> /scripts/.apacheenv
+    fi
 fi
 
 #Need escaped regex
-appregex=$(echo "$APPLICATION_CNAME" | sed 's/\./\\./g')
-export APPLICATION_CNAME_REGEX=$appregex
+if [ -z "$APPLICATION_CNAME_REGEX" ]; then
+    appregex=$(echo "$APPLICATION_CNAME" | sed 's/\./\\./g')
+    export APPLICATION_CNAME_REGEX=$appregex
+
+    touch /scripts/.apacheenv
+    if grep -Fxq "$ex" /scripts/.apacheenv
+    then
+        echo "CNAME_REGEX already in .apacheenv"
+    else
+        ex="export APPLICATION_CNAME_REGEX=$appregex"
+        echo "$ex" >> /scripts/.apacheenv
+    fi
+fi
 
 #Ensure tmp writeable
 chmod a+w /tmp
